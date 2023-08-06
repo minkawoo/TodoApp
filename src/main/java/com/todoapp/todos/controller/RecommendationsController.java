8d71c21cc1e00df9b25be2ce231b23b1e71f81a5
minkawoo <minkawoo@gmail.com>
2023-08-06 20:38:36 +0900

    Second

--- src/main/java/com/todoapp/todos/controller/RecommendationsController.java
+++ src/main/java/com/todoapp/todos/controller/RecommendationsController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecommendationsController {
     @GetMapping("/hello")
     public String helloWorld() {
-        return "To-do Application !";
+        return "To-do Application !!";
     }
 }
